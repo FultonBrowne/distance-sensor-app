3315c5bf9d3ae57a6f884ea18d3105fcb9e3a747
Fulton Browne <fultonbrowne@pm.me>
2024-09-16 12:55:40 -0500

    Initial Commit

---
+++ distance-sensor/distance_sensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  distance_sensorApp.swift
+//  distance-sensor
+//
+//  Created by Fulton Browne on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct distance_sensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
